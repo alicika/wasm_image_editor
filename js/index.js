4bc7f3417759bc92e69f1db399ed058d0cba06d9
alicika <52126032+alicika@users.noreply.github.com>
2021-10-11 23:09:09 +0900

    chore: adding a function with canvas API

--- js/index.js
+++ js/index.js
@@ -1,5 +1,46 @@
+function draw(image) {
+    const canvas = document.getElementById("my-canvas");
+    const context = canvas.getContext("2d");
+    //context.fillStyle = "red";
+    //context.fillRect(0, 0, 50, 50);
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+
+    const width = image.width();
+    const height = image.height();
+    const cellSize = 50;
+
+    const cells = image.cells();
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const index = ((y * width) + x) * 3;
+            const colour = `rgb(${cells[index]}, ${cells[index + 1]}, ${cells[index + 2]})`;
+            context.fillStyle = colour;
+            context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+        }
+    }
+
+    for (let x = 0; x <= width; x++) {
+        context.beginPath();
+        context.moveTo(x * cellSize, 0);
+        context.moveTo(x * cellSize, height * cellSize);
+        context.stroke();
+    }
+
+    for (let y = 0; y <= width; y++) {
+        context.beginPath();
+        context.moveTo(y * cellSize, 0);
+        context.moveTo(y * cellSize, height * cellSize);
+        context.stroke();
+    }
+}
+
 async function main() {
     const lib = await import("../pkg/index.js").catch(console.error);
-    const image = new lib.Image;
+    const image = new lib.Image(10, 10);
     console.log(image);
+    draw(image);
 }
+
+main();
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -10,12 +9,46 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Clone, Copy)]
+struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
 #[wasm_bindgen]
-pub struct Image {}
+pub struct Image {
+    width: usize,
+    height: usize,
+    cells: Vec<Rgb>,
+}
 
 #[wasm_bindgen]
 impl Image {
-    pub fn new() -> Image {
-        Image {}
+    #[wasm_bindgen]
+    pub fn new(width: usize, height: usize) -> Image {
+        let mut cells = Vec::new();
+        cells.resize(width * height, Rgb{r: 200, g:200, b:255});
+        Image {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn cells(&self) -> Vec<u8> {
+        self.cells
+            .iter()
+            .flat_map(|&rgb| vec![rgb.r, rgb.g, rgb.b])
+            .collect::<Vec<u8>>()
     }
 }
+
