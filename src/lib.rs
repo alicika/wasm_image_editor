027092f9b416da4d26f1545c6be75328b40a56d8
alicika <52126032+alicika@users.noreply.github.com>
2021-10-18 22:50:34 +0900

    chore: add untar

--- src/lib.rs
+++ src/lib.rs
@@ -59,5 +59,20 @@ impl Image {
             b: color[2],
         }
     }
+
+    use std::fs::File;
+    use flate2::read::GzDecoder;
+    use tar::Archive;
+
+    pub fn untar() -> Result<(), std::io::Error> {
+        let path = "archive.tar.gz";
+
+        let tar_gz = File::open(path)?;
+        let tar = GzDecoder::new(tar_gz);
+        let mut archive = Archive::new(tar);
+        archive.unpack(".")?;
+
+        Ok(())
+    }
 }
 
